6886ff8072dfc8affbdfa7f6dc88f5ca2b560399
Hemant Kumar <hekumar@redhat.com>
2019-07-09 16:31:26 -0400

    Add support for overriding diskmaker image
    Add an example of using block volume

--- hack/test-e2e.sh
+++ hack/test-e2e.sh
@@ -5,21 +5,26 @@ ARTIFACT_DIR=${ARTIFACT_DIR:-_output}
 manifest=$(mktemp)
 global_manifest=$(mktemp)
 
+# save generated manifest files in _output directory for debugging purposes
 cleanup(){
   local return_code="$?"
   set +e
 
-  cat $manifest > $ARTIFACT_DIR/manifest
-  cat $global_manifest > $ARTIFACT_DIR/global_manifest
-
+  cp ${manifest} $ARTIFACT_DIR/manifest
+  cp ${global_manifest} $ARTIFACT_DIR/global_manifest
+  rm -rf ${manifest}
+  rm -rf ${global_manifest}
   exit $return_code
 }
+
 trap cleanup exit
 
-if [ -n "${IMAGE_FORMAT:-}" ] ; then
-  IMAGE_LOCAL_STORAGE_OPERATOR=$(sed -e "s,\${component},local-storage-operator," <(echo $IMAGE_FORMAT))
+if [ -n "${IMAGE_FORMAT}" ] ; then
+    IMAGE_LOCAL_STORAGE_OPERATOR=$(sed -e "s,\${component},local-storage-operator," <(echo $IMAGE_FORMAT))
+    IMAGE_LOCAL_DISKMAKER=$(sed -e "s,\${component},local-storage-diskmaker," <(echo $IMAGE_FORMAT))
 else
-  IMAGE_LOCAL_STORAGE_OPERATOR=${IMAGE_LOCAL_STORAGE_OPERATOR:-quay.io/openshift/origin-local-storage-operator}
+    IMAGE_LOCAL_STORAGE_OPERATOR=${IMAGE_LOCAL_STORAGE_OPERATOR:-quay.io/openshift/origin-local-storage-operator}
+    IMAGE_LOCAL_DISKMAKER=${IMAGE_LOCAL_DISKMAKER:-quay.io/openshift/origin-local-storage-diskmaker}
 fi
 
 KUBECONFIG=${KUBECONFIG:-$HOME/.kube/config}
@@ -30,6 +35,7 @@ cat ${repo_dir}/deploy/operator.yaml >> ${manifest}
 cat ${repo_dir}/deploy/crd.yaml >> ${global_manifest}
 
 sed -i "s,quay.io/openshift/origin-local-storage-operator,${IMAGE_LOCAL_STORAGE_OPERATOR}," ${manifest}
+sed -i "s,quay.io/openshift/origin-local-storage-diskmaker,${IMAGE_LOCAL_DISKMAKER}," ${manifest}
 
 TEST_NAMESPACE=${NAMESPACE} go test ./test/e2e/... \
   -root=$(pwd) \
