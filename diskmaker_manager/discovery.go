6999b1e09913f1ea864cdb5dbc950a997bcaf18f
Santosh Pillai <sapillai@redhat.com>
2021-07-08 14:25:37 +0530

    add local volume discovery metric for total discovered disks
    Signed-off-by: Santosh Pillai <sapillai@redhat.com>

--- diskmaker_manager/discovery.go
+++ diskmaker_manager/discovery.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"github.com/openshift/local-storage-operator/diskmaker/discovery"
+	"github.com/openshift/local-storage-operator/localmetrics"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
 func startDeviceDiscovery(cmd *cobra.Command, args []string) error {
 	printVersion()
+	// start local server to emit custom metrics
+	err := localmetrics.NewConfigBuilder().
+		WithCollectors(localmetrics.LVDMetricsList).
+		Build()
+	if err != nil {
+		return errors.Wrap(err, "failed to discover devices")
+	}
+
 	discoveryObj, err := discovery.NewDeviceDiscovery()
 	if err != nil {
-		return errors.Wrapf(err, "failed to discover devices")
+		return errors.Wrap(err, "failed to discover devices")
 	}
+
 	err = discoveryObj.Start()
 	if err != nil {
-		return errors.Wrapf(err, "failed to discover devices")
+		return errors.Wrap(err, "failed to discover devices")
 	}
+
 	return nil
 }
