c51e97ea87be538fa94e89a40a0a62969f278995
Hemant Kumar <hekumar@redhat.com>
2022-03-03 08:01:14 -0500

    Use device location by user

--- diskmaker/controllers/lv/reconcile_test.go
+++ diskmaker/controllers/lv/reconcile_test.go
@@ -139,7 +139,7 @@ func TestCreateSymLinkByDeviceID(t *testing.T) {
 		},
 	}
 	d, _ := getFakeDiskMaker(t, tmpSymLinkTargetDir, lv, sc)
-	diskLocation := DiskLocation{fakeDisk.Name(), fakeDiskByID.Name(), internal.BlockDevice{}}
+	diskLocation := DiskLocation{fakeDisk.Name(), fakeDiskByID.Name(), fakeDisk.Name(), internal.BlockDevice{}}
 
 	d.runtimeConfig = &provCommon.RuntimeConfig{
 		UserConfig: &provCommon.UserConfig{
@@ -185,7 +185,7 @@ func TestCreateSymLinkByDeviceName(t *testing.T) {
 	}
 
 	d, _ := getFakeDiskMaker(t, tmpSymLinkTargetDir, lv, sc)
-	diskLocation := DiskLocation{fakeDisk.Name(), "", internal.BlockDevice{}}
+	diskLocation := DiskLocation{fakeDisk.Name(), "", fakeDisk.Name(), internal.BlockDevice{}}
 	d.createSymlink(diskLocation, fakeDisk.Name(), path.Join(tmpSymLinkTargetDir, "diskName"), false)
 
 	// assert that target symlink is created for disk name when no disk ID is available
