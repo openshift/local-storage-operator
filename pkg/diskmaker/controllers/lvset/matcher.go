0c6697df889c1e7cf0c958ba90b291f74a70b999
Satoru Sato <satoru.satoh@gmail.com>
2026-06-28 02:35:29 +0900

    lvset: add filter for selecting disks with specified serials
    Signed-off-by: Satoru SATOH <satoru.satoh@gmail.com>

--- pkg/diskmaker/controllers/lvset/matcher.go
+++ pkg/diskmaker/controllers/lvset/matcher.go
@@ -30,6 +30,7 @@ const (
 	inMechanicalPropertyList = "inMechanicalPropertyList"
 	inVendorList             = "inVendorList"
 	inModelList              = "inModelList"
+	inSerialList             = "inSerialList"
 
 	// exclusion matcher names:
 	notInDeviceNameFilter = "notInDeviceNameFilter"
@@ -199,6 +200,30 @@ var matcherMap = map[string]func(internal.BlockDevice, *localv1alpha1.DeviceIncl
 			}
 		}
 		return matched, nil
+	},
+	inSerialList: func(dev internal.BlockDevice, spec *localv1alpha1.DeviceInclusionSpec) (bool, error) {
+		if spec == nil {
+			return true, nil
+		}
+                if len(spec.Serials) == 0 {
+                        return true, nil
+                }
+                deviceSerial := strings.TrimSpace(dev.Serial)
+                hasConfiguredSerial := false
+                for _, serial := range spec.Serials {
+                        serial = strings.TrimSpace(serial)
+                        if serial == "" {
+                                continue
+                        }
+                        hasConfiguredSerial = true
+                        if strings.EqualFold(deviceSerial, serial) {
+                                return true, nil
+                        }
+                }
+                if !hasConfiguredSerial {
+                        return true, nil
+                }
+                return false, nil
 	},
 }
 
